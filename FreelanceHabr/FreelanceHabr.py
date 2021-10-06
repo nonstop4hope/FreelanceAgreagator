70f22bb216a14dbabdb7f4779adac7a43d623268
nonstop4hope <igormitusov@mail.ru>
2021-10-06 20:35:37 +0300

    Convert dataclass to json

--- FreelanceHabr/FreelanceHabr.py
+++ FreelanceHabr/FreelanceHabr.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-from FreelanceHabr.models import Task, HabrTasks
+from FreelanceHabr.models import Task
 from urllib.parse import urljoin
+from time import sleep
+import random
 
 
 class FreelanceHabr:
 
     def get_habr_task_description_and_date(self, url):
-
         description = ''
         date = ''
         response = requests.get(url)
@@ -15,14 +16,13 @@ class FreelanceHabr:
             soup = BeautifulSoup(response.text, 'html.parser')
             description = soup.find('div', class_='task__description').text.strip()
             date = soup.find('div', class_='task__meta').text.split(' • ')[0].strip()
+            sleep(random.randint(2, 5))
             return description, date
         else:
             return description, date
 
     def get_habr_tasks(self, url: str = 'https://freelance.habr.com/tasks'):
-
-        habr_tasks = HabrTasks()
-
+        habr_tasks = []
         habr_response = requests.get(url)
         if habr_response.status_code == 200:
             soup = BeautifulSoup(habr_response.text, 'html.parser')
@@ -31,9 +31,11 @@ class FreelanceHabr:
 
             for task in tasks:
                 habr_task = Task()
-
+                habr_task.site = 'habr.com'
+                habr_task.ID = task.find('div', class_='task__title').find('a').get('href').split('/')[2]
                 habr_task.name = task.find('div', class_='task__title').get('title')
                 habr_task.url = urljoin(url, task.find('div', class_='task__title').find('a').get('href'))
+
                 habr_task.views = task.find('span', class_='params__views icon_task_views').find('i').text
                 habr_task.price = task.find('div', class_='task__price').text
 
@@ -52,7 +54,9 @@ class FreelanceHabr:
                 for tag in tags_list:
                     habr_task.tags.append(tag.find('a').text)
 
-                habr_tasks.tasks.append(habr_task)
+                habr_tasks.append(habr_task.to_json())
+
+                break
 
         return habr_tasks
 
--- FreelanceHabr/models.py
+++ FreelanceHabr/models.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass, field
 from typing import List
+import json
 
 
 @dataclass
 class Task:
+    site: str = ''
+    ID: str = ''
     name: str = ''
     description: str = ''
     tags: List[str] = field(default_factory=list)
@@ -13,7 +16,9 @@ class Task:
     responses: str = ''
     date: str = ''
 
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, ensure_ascii=False, indent=4)
 
-@dataclass
-class HabrTasks:
-    tasks: List[Task] = field(default_factory=list)
+
+
+
